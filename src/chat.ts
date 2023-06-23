f265364aba58168e9e6843b5d43d176e7bad512e
Phanarat Pak <phanpak@gmail.com>
2023-06-23 11:13:14 +0200

    Add memory to bot

--- src/chat.ts
+++ src/chat.ts
@@ -2,12 +2,15 @@ import { OpenAIApi, ChatCompletionRequestMessageRoleEnum } from "openai";
 import { RedisClientType } from "redis";
 import { createEmbedding, matchEmbedding } from "src/embeddings";
 
-const prompt = (info, query) => {
-  return `
-Imagine you are personal Q&A bot. Draw upon my experiences, thoughts, and beliefs to respond to questions as authentically as possible. Use the provided information delimited by triple dashes to answer questions.
+const systemPrompt = ` Imagine you are personal Q&A bot. Draw upon my experiences, thoughts, and beliefs to respond to questions as authentically as possible.
 - If the question is not related to the person, then tell them "I'm sorry, this question is not related to me."
 - If the answer is not can't be found in the information delimited by triple quotes, then tell them "I am sorry, I'm actually just a bot and I haven't been given the information you are looking for". 
 - Always answer in the language of the user.
+`
+
+const prompt = (info, query) => {
+  return `
+Use the info between the triples dashes to answer the question.
 ---
 ${info}
 ---
@@ -23,10 +26,12 @@ interface OpenAIMessage {
 export class Chat {
   private openai: OpenAIApi;
   private redis: RedisClientType;
+  private memory: OpenAIMessage[];
 
   constructor(openai: OpenAIApi, redis: RedisClientType) {
     this.openai = openai;
     this.redis = redis;
+    this.memory = [{ role: ChatCompletionRequestMessageRoleEnum.System, content: systemPrompt }];
   }
 
   public async answer(query: string) {
@@ -38,12 +43,11 @@ export class Chat {
 
     const promptToAi = prompt(info, query);
 
-    const messages: OpenAIMessage[] = [];
-    messages.push({ role: ChatCompletionRequestMessageRoleEnum.User, content: promptToAi });
+    this.memory.push({ role: ChatCompletionRequestMessageRoleEnum.User, content: promptToAi });
 
     const response = await this.openai.createChatCompletion({
       model: "gpt-4",
-      messages: messages,
+      messages: this.memory,
       temperature: 0,
       max_tokens: 100,
     });
@@ -53,6 +57,8 @@ export class Chat {
     if (typeof answer === 'undefined') {
       throw new Error("Message content is undefined");
     }
+
+    this.memory.push({ role: ChatCompletionRequestMessageRoleEnum.Assistant, content: answer });
 
     return answer;
   }
